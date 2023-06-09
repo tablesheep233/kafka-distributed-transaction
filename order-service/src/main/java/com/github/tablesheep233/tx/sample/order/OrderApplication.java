480bc937f8d9741415a1912f1ee7793a5cb32b22
tablesheep233 <858916094@qq.com>
2023-06-09 17:31:26 +0800

    init project

---
+++ order-service/src/main/java/com/github/tablesheep233/tx/sample/order/OrderApplication.java
@@ -0,0 +1,21 @@
+package com.github.tablesheep233.tx.sample.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Order application.
+ *
+ * @author tablesheep233
+ */
+@SpringBootApplication
+public class OrderApplication {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
